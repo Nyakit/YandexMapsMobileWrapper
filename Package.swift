1a31bac44398299dec8cbf8739335d27dbc2bb52
Nyakit <nyakitenno@gmail.com>
2021-08-04 10:05:24 +0500

    unnecessary dependency removed

--- Package.swift
+++ Package.swift
@@ -24,11 +24,12 @@ let package = Package(
         ),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .target(
             name: "YandexMapsMobileWrapper",
-            dependencies: [
-                .target(name: "YandexMapsMobileBinary"),
-            ],
+//            dependencies: [
+//                .target(name: "YandexMapsMobileBinary"),
+//            ],
             linkerSettings: [
                 .linkedFramework("CoreLocation"),
                 .linkedFramework("CoreTelephony"),
