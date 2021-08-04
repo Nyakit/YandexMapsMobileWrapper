bbd6c2de9646a6c604d768910e9a16b271100a79
Nyakit <nyakitenno@gmail.com>
2021-08-04 12:03:24 +0500

    test

--- Package.swift
+++ Package.swift
@@ -6,25 +6,19 @@ import PackageDescription
 let package = Package(
     name: "YandexMapsMobileWrapper",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YandexMapsMobileWrapper",
-            targets: ["YandexMapsMobileWrapper"]),
+            targets: ["YandexMapsMobileWrapper", "YandexMapsMobile"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        
     ],
     targets: [
-        //.binaryTarget(name: "YandexMapsMobileBinary", path: "YandexMapsMobile.xcframework"),
         .binaryTarget(
             name: "YandexMapsMobile",
             url: "https://github.com/Nyakit/YandexMapsMobileWrapper/releases/download/1.0.0/YandexMapsMobile.xcframework.zip",
             checksum: "90b4402623a7a10ec57538db320ff132414a59e8a517e205181d8ae97f7c166e"
         ),
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        
         .target(
             name: "YandexMapsMobileWrapper",
             dependencies: [
